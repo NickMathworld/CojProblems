4f4525f8aa229a8b79f8716eb083e1d3a6d822ff
Brian Zuñiga Uribe <NickMathworld@users.noreply.github.com>
2019-04-08 22:41:53 -0500

    Commit_18

--- Codefights/dings.cpp
+++ Codefights/dings.cpp
@@ -1,5 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+class backpack{
+    private:
+        string nombre;
+        string apellido_paterno;
+        string apellido_materno;
+        int edad;
+        double estatura;
+    public:
+        backpack(string _nombre,string _apellido_paterno, string _apellido_materno, int _edad, double _estatura)
+        :nombre(_nombre),
+        apellido_paterno(_apellido_paterno),
+        apellido_materno(_apellido_materno),
+        edad(_edad),estatura(_estatura){
+            cout << "Object created" << endl;
+        }
+        string to_string(){
+            return nombre+" "+apellido_paterno+" "+apellido_materno;
+        }
+};
 int main(){
-	
+    cout << "Hola mundo";
+    backpack mochila = *(new backpack("nick","zuniga","uribe",24,1.70));
+    cout << mochila.to_string() << endl;
+    return 0;
 }
